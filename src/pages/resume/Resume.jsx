61f1051e6194de3e827a780fd881e7914395cd4e
ashaansojib <ashaduzzamansojib67@g.gmail.com>
2023-06-21 14:08:55 +0600

    add aos

--- src/pages/resume/Resume.jsx
+++ src/pages/resume/Resume.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Skills from './Skills';
 import Knoledge from './Knoledge';
+import Testimonial from './Testimonial';
 
 const Resume = () => {
     return (
-        <div className='text-gray-700'>
+        <div className='text-gray-700' data-aos="fade-right">
             <Skills />
             <Knoledge />
+            <Testimonial />
         </div>
     );
 };
---
+++ src/pages/resume/Testimonial.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Testimonial = () => {
+    return (
+        <div>
+            <h2 className='text-2xl font-medium border-b py-4'><span className='text-green-500'>T</span>estimonials</h2>
+            <div></div>
+        </div>
+    );
+};
+
+export default Testimonial;
